1b2391cde27ace955055c915cebdd3af3df1746a
Jsons21 <jakesonsini@gmail.com>
2025-06-12 22:46:52 -0400

    count

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ const arr = userInput.split(',');
 userObject.order = userInput
 console.log(userInput.split(','));
 
+const count = {}
+for (let i = 0; i < arr.length; i++) {
+  const element = arr[i];
+
+  if (count[element]) {
+    count[element] += 1;
+  } else {
+    count[element] = 1;
+  }
+}
+console.log(count);
 
 
 
